a3606b2bf7d83586fb0279aba796b6f1e30663cd
Dominik Mrajca <racingpark54@gmail.com>
2019-04-27 19:13:20 +0200

    Better user searching

--- commands/User.js
+++ commands/User.js
@@ -2,27 +2,95 @@ const Discord = require("discord.js");
 
 module.exports = {
 	name: "user",
+	aliases: ["member"],
+	syntax: ["% [user]"],
+	group: "info",
 	description: "Display some information about your profile or the profile of a specific user. GalaxyBot owner can search for users in all servers.",
 
 	execute: command => {
 		let targetUser = command.user, targetMember = command.member, argument;
 		const extendedSearch = command.user.id == command.galaxybot.config.owner;
+		let searchByID = "";
 
 		if (command.arguments.length >= 1) {
 			argument = command.galaxybot.escapeMentions(command.arguments.join(" "), command.message.mentions);
+			searchByID = argument.match(/[0-9]*$/)[0];
+		}
+
+		function sendUserInfo(user, member) {
+			// User not found.
+			if (user === undefined) {
+				command.channel.send(`Sorry ${command.user}, I couldn't find the user **${argument}**. :rolling_eyes:`);
+				command.botGuild.log(`Could not find user "${argument}".`);
+				return;
+			}
+
+			// Initialize fields array with a creation date.
+			let fields = [{
+				name: (user.bot ? "Created at" : "Discord user since"),
+				value: command.galaxybot.formatDate(user.createdAt),
+				inline: true
+			}];
+
+			// If member of a guild, show how long the person is a member.
+			if (member) {
+				fields.push({
+					name: "Server member since",
+					value: command.galaxybot.formatDate(member.joinedAt),
+					inline: true
+				});
+
+				let roles = [];
+
+				member.roles.forEach((role, roleID) => {
+					if (role.calculatedPosition <= 0) return;
+					roles.push(`${role}`);
+				});
+
+				// Show the roles of that person.
+				if (roles.length > 0) fields.push({
+					name: "Roles",
+					value: roles.join(", ")
+				});
+			}
+
+			// Format the presence text.
+			const statuses = { online: "Online", offline: "Offline", idle: "Idle", dnd: "Do Not Disturb" };
+			const gameType = ["Playing", "Streaming", "Listening to", "Watching"];
+
+			command.channel.send(new Discord.RichEmbed({
+				author: {
+					name: user.tag,
+					icon_url: user.displayAvatarURL
+				},
+				description: (user.presence.game ? `${gameType[user.presence.game.type]} **${user.presence.game.name}**` : statuses[user.presence.status]),
+				fields: fields,
+				color: (member && member.displayColor > 0 ? member.displayColor : undefined),
+				thumbnail: {
+					url: user.displayAvatarURL
+				},
+				footer: {
+					text: "ID: " + user.id
+				}
+			}));
+
+			command.botGuild.log("Showing user information: " + user.tag);
 		}
 
 		// User search.
 		if (argument) {
+			targetUser = undefined;
+			targetMember = undefined;
+
 			// Find the matching member if in a guild and specified.
 			if (command.guild) {
-				targetUser = undefined;
 				targetMember = command.botGuild.findMember(argument, command.message.mentions);
+				const showError = !extendedSearch && searchByID == "";
 
 				if (targetMember) targetUser = targetMember.user;
 
 				// User not found.
-				if (targetMember === undefined && !extendedSearch) {
+				if (targetMember === undefined && showError) {
 					command.channel.send(`Sorry ${command.user}, I couldn't find the user **${argument}** on this server. :rolling_eyes:`);
 					command.botGuild.log(`Could not find user "${argument}".`);
 					return;
@@ -30,67 +98,24 @@ module.exports = {
 			}
 
 			// Owner only: find any user.
-			if (extendedSearch) {
+			if (targetMember === undefined && extendedSearch) {
 				targetUser = command.galaxybot.findUser(argument, command.message.mentions);
 			}
-		}
 
-		// User not found.
-		if (targetUser === undefined) {
-			command.channel.send(`Sorry ${command.user}, I couldn't find the user **${argument}**. :rolling_eyes:`);
-			command.botGuild.log(`Could not find user "${argument}".`);
-			return;
-		}
-
-		// Initialize fields array with a creation date.
-		let fields = [{
-			name: (targetUser.bot ? "Created at" : "Discord user since"),
-			value: command.galaxybot.formatDate(targetUser.createdAt),
-			inline: true
-		}];
-
-		// If member of a guild, show how long the person is a member.
-		if (targetMember) {
-			fields.push({
-				name: "Server member since",
-				value: command.galaxybot.formatDate(targetMember.joinedAt),
-				inline: true
-			});
-
-			let roles = [];
-
-			targetMember.roles.forEach((role, roleID) => {
-				if (role.calculatedPosition <= 0) return;
-				roles.push(`${role}`);
-			});
-
-			// Show the roles of that person.
-			if (roles.length > 0) fields.push({
-				name: "Roles",
-				value: roles.join(", ")
-			});
-		}
-
-		// Format the presence text.
-		const statuses = { online: "Online", offline: "Offline", idle: "Idle", dnd: "Do Not Disturb" };
-		const gameType = ["Playing", "Streaming", "Listening to", "Watching"];
-
-		command.channel.send(new Discord.RichEmbed({
-			author: {
-				name: targetUser.tag,
-				icon_url: targetUser.displayAvatarURL
-			},
-			description: (targetUser.presence.game ? `${gameType[targetUser.presence.game.type]} **${targetUser.presence.game.name}**` : statuses[targetUser.presence.status]),
-			fields: fields,
-			color: (targetMember && targetMember.displayColor > 0 ? targetMember.displayColor : undefined),
-			thumbnail: {
-				url: targetUser.displayAvatarURL
-			},
-			footer: {
-				text: "ID: " + targetUser.id
+			// Search by user ID
+			if (targetUser === undefined && searchByID) {
+				command.galaxybot.client.fetchUser(searchByID).then(user => {
+					sendUserInfo(user);
+				}).catch(error => {
+					command.channel.send(`Sorry ${command.user}, I couldn't find the user with ID **${argument}**. :rolling_eyes:`);
+					command.botGuild.log(`Could not find user "${argument}".`);
+				});
 			}
-		}));
+		}
 
-		command.botGuild.log("Showing user information: " + targetUser.tag);
+		// User has been found.
+		if (targetUser) {
+			sendUserInfo(targetUser, targetMember);
+		}
 	}
 }
